442874a85d2df58fd7621bccc9da8e1677b91c83
tiannian <dtiannian@gmail.com>
2022-02-08 11:55:07 +0800

    format code

--- contracts/OVR.sol
+++ contracts/OVR.sol
@@ -7,22 +7,22 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract OVR is ERC20Upgradeable, OwnableUpgradeable {
-	address public rewards;
+    address public rewards;
 
     modifier onlyRewards {
         require(msg.sender == rewards);
         _;
     }
 
-	function initialize() public initializer {
-		// Do intital.
-	}
+    function initialize() public initializer {
+        // Do intital.
+    }
 
-	function adminChangeRewards(address _rewards) onlyOwner public {
-		rewards = _rewards;
-	}
+    function adminChangeRewards(address _rewards) onlyOwner public {
+        rewards = _rewards;
+    }
 
-	function mintByRewards(address target, uint256 amount) public onlyRewards {
-	  _mint(target, amount);
-	}
+    function mintByRewards(address target, uint256 amount) public onlyRewards {
+        _mint(target, amount);
+    }
 }
